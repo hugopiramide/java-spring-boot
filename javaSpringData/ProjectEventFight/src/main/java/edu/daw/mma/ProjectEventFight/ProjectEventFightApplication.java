9710bc0446e113ab817db3e2385ca2e8a5b4a8b1
hugopiramide <hugocastillo.deus@gmail.com>
2025-10-27 16:23:19 +0100

    Add new feature EventsFight

---
+++ javaSpringData/ProjectEventFight/src/main/java/edu/daw/mma/ProjectEventFight/ProjectEventFightApplication.java
@@ -0,0 +1,13 @@
+package edu.daw.mma.ProjectEventFight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEventFightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEventFightApplication.class, args);
+	}
+
+}
---
+++ javaSpringData/ProjectEventFight/src/test/java/edu/daw/mma/ProjectEventFight/ProjectEventFightApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.daw.mma.ProjectEventFight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEventFightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
