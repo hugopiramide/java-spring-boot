24b46b296942bac6f2472c6f25556259a9133887
hugopiramide <hugocastillo.deus@gmail.com>
2025-10-29 16:40:17 +0100

    Update contrato_luchador_entrenador, issue solved

--- javaSpringData/ProjectEventFight/src/main/java/edu/daw/mma/ProjectEventFight/model/Contrato_luchador_entrenador.java
+++ javaSpringData/ProjectEventFight/src/main/java/edu/daw/mma/ProjectEventFight/model/Contrato_luchador_entrenador.java
@@ -3,13 +3,17 @@ package edu.daw.mma.ProjectEventFight.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "contrato_luchador_entrenador")
 public class Contrato_luchador_entrenador {
 
     @Id
