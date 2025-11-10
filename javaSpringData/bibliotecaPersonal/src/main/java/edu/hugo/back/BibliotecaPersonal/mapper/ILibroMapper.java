3001333811ce6d306ad037f06f0e5af1b3538248
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-10 17:17:35 +0100

    Add @Mapping, libros to DTO and DTO to libros

--- javaSpringData/bibliotecaPersonal/src/main/java/edu/hugo/back/BibliotecaPersonal/mapper/ILibroMapper.java
+++ javaSpringData/bibliotecaPersonal/src/main/java/edu/hugo/back/BibliotecaPersonal/mapper/ILibroMapper.java
@@ -1,8 +1,25 @@
 package edu.hugo.back.BibliotecaPersonal.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import edu.hugo.back.BibliotecaPersonal.model.Libro;
+import edu.hugo.back.BibliotecaPersonal.model.dto.LibroDTO;
 
 @Mapper(componentModel = "spring")
 public interface ILibroMapper {
 
+    @Mapping(target = "isbn", expression = "java(new Isbn(dto.isbn()))")
+    @Mapping(target = "precio", expression = "java(new Money(dto.precio(), dto.moneda()))")
+    Libro toEntity(LibroDTO dto);
+
+    @Mapping(source = "isbn.value", target = "isbn")
+    @Mapping(source = "precio.mount", target = "precio")
+    @Mapping(source = "precio.currency", target = "moneda")
+    LibroDTO toDto(Libro libro);
+
+    @Mapping(target = "isbn", expression = "java(new Isbn(dto.isbn()))")
+    @Mapping(target = "precio", expression = "java(new Money(dto.precio(), dto.moneda()))")
+    void updateLibroFromDto(LibroDTO dto, @MappingTarget Libro entity);
 }
