b5f206d633b25ef55934f59b86f1d15dd92d3bdd
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-13 12:06:16 +0100

    Add controller file

---
+++ javaSpringData/bibliotecaPersonal/src/main/java/edu/hugo/back/BibliotecaPersonal/controller/LibroController.java
@@ -0,0 +1,10 @@
+package edu.hugo.back.BibliotecaPersonal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/libros")
+public class LibroController {
+
+}
